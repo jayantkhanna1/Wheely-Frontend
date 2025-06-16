c4750b83b30954b0e0e6411cbd64326f810c64c5
Ananya Vishnoi <71982267+ananya26-vishnoi@users.noreply.github.com>
2025-06-17 02:36:14 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Chrome as Home, Car, Bike, Bicycle } from 'lucide-react-native';
+import { Chrome as Home, Car, Bike, Recycle as Bicycle } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
