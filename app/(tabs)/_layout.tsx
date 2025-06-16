eb2d33c19a8559dee747538a647350c4647a009e
Ananya Vishnoi <71982267+ananya26-vishnoi@users.noreply.github.com>
2025-06-16 23:49:20 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home } from 'lucide-react-native';
+import { Chrome as Home } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
